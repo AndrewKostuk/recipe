16d957badb22beea8ee0567c0f9497f2e2362122
AndrewKostuk <andrkost78@gmail.com>
2023-08-05 22:44:37 +0300

    start refactoring code with lombok annotations

--- src/main/java/by/bsuir/andrei/recipe/services/RecipeServiceImpl.java
+++ src/main/java/by/bsuir/andrei/recipe/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package by.bsuir.andrei.recipe.services;
 
 import by.bsuir.andrei.recipe.domain.Recipe;
 import by.bsuir.andrei.recipe.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
@@ -17,6 +19,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public List<Recipe> getAll() {
+        log.debug("I'm in the Recipe Service");
         return recipeRepository.findAll();
     }
 }
